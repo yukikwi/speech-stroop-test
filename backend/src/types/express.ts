af96f8b82047e847a655020270fd2cf43edc716c
Pachara Chantawong <66879660+yukikwi@users.noreply.github.com>
2022-10-12 13:48:53 +0700

    Backend: Add type for experimental's router request

--- backend/src/types/express.ts
+++ backend/src/types/express.ts
@@ -1,6 +1,11 @@
 import { Request } from 'express'
+import { experimentalDocument } from 'src/model/experimental'
 import { UserDocument } from '../model/user'
 
 export type RequestWithUser = Request & {
   user: UserDocument
+}
+
+export type RequestWithExperimental = Request & {
+  experimental: experimentalDocument
 }
